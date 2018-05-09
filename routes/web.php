4c338e88d2c42489fe577a3df730c3475deb532b
Joseph Matembu <jmatembu@gmail.com>
2018-05-09 15:17:40 +0300

    Add home route

--- routes/web.php
+++ routes/web.php
@@ -10,6 +10,8 @@
 | contains the "web" middleware group. Now create something great!
 |
 */
-
+Route::get('/', function () {
+	return view('welcome');
+});
 Route::get('/concerts/{id}', 'ConcertsController@show');
 Route::post('/concerts/{id}/orders', 'ConcertOrdersController@store');
